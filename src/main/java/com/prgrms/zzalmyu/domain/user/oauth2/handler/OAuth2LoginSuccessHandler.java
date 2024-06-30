e31da06511d2c031b00306b3f5854c940180d441
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-01 02:55:32 +0900

    [FIX] redirect url 수정

--- src/main/java/com/prgrms/zzalmyu/domain/user/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/prgrms/zzalmyu/domain/user/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -33,7 +33,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         jwtService.updateRefreshToken(refreshToken, email);
 
         //응답 보내기
-        response.sendRedirect(UriComponentsBuilder.fromUriString("https://zzalmyu.site")
+        response.sendRedirect(UriComponentsBuilder.fromUriString("https://zzalmyu.asia")
             .queryParam("accessToken", accessToken)
             .queryParam("refreshToken", refreshToken)
             .build()
